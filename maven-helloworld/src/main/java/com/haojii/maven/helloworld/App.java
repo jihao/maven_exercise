a45df3d046c436f9c20ef2605309259f7b15b7a8
jihao <jacky.jihao@gmail.com>
2011-09-05 15:30:04 +0800

    exercise1

--- maven-helloworld/src/main/java/com/haojii/maven/helloworld/App.java
+++ maven-helloworld/src/main/java/com/haojii/maven/helloworld/App.java
@@ -1,13 +1,18 @@
 package com.haojii.maven.helloworld;
 
+import javax.servlet.http.Cookie;
+
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		javax.servlet.http.Cookie c = new Cookie("key", "value");
+		Logger logger =Logger.getLogger(App.class);
+		logger.debug(c.getVersion());
+	}
 }
