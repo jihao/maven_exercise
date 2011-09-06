054047fb3eecdef7f09c5d2bca3fdbb878892429
jihao <jacky.jihao@gmail.com>
2011-09-06 15:09:38 +0800

    add module and dependencyManagement

---
+++ maven-helloworld/helloworld-module/src/main/java/ModuleMainApp.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+
+public class ModuleMainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Logger logger =Logger.getLogger(ModuleMainApp.class);
+		logger.debug(ModuleMainApp.class.toString());
+
+	}
+
+}
--- maven-helloworld/src/main/java/com/haojii/maven/helloworld/App.java
+++ maven-helloworld/src/main/java/com/haojii/maven/helloworld/App.java
@@ -2,8 +2,6 @@ package com.haojii.maven.helloworld;
 
 import javax.servlet.http.Cookie;
 
-import org.apache.log4j.Logger;
-
 /**
  * Hello world!
  * 
@@ -12,7 +10,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		javax.servlet.http.Cookie c = new Cookie("key", "value");
-		Logger logger =Logger.getLogger(App.class);
-		logger.debug(c.getVersion());
+		
 	}
 }
